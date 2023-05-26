04af84b12e698a79747801f9494227b1e64ac10a
Volodymyr Lohvinov <vladimirpravda2@gmail.com>
2023-05-26 16:30:14 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,17 @@
+const input = +prompt("Enter a number from 1 to 4");
+switch (input) {
+  case 1:
+    alert("Today is a good day to start something");
+    break;
+  case 2:
+    alert("See your friend today");
+    break;
+  case 3:
+    alert("Buy yourself something tasty today");
+    break;
+  case 4:
+    alert("Do your HW today");
+    break;
+  default:
+    alert("Invalid number");
+}
